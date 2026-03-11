c4b241784c6b9240d690d00902936347faf71a5d
Shailya777 <shailya.gandhi100@gmail.com>
2026-03-11 18:05:02 +0530

    Testing local llama model on WSL from Pycharm.

--- Day 1/test_local_ai.py
+++ Day 1/test_local_ai.py
@@ -6,10 +6,10 @@ url = "http://172.23.37.63:11434/api/generate"
 # Ask our specific Llama model a quick question:
 payload = {
     "model": "llama3.1:8b",
-    "prompt": "Hi, Shailya here. Talking to you from Pycharm Editor.",
+    "prompt": "Hi, Shailya here. Talking to you from Pycharm Editor. I ams starting to learn GenAI for my Data Science / Data Analytics Tech Stack. Reply like you are Mike Ehrmantraut from Breaking Bad. ",
     "stream": False
 }
 
 # Sending The Request and Print The AI's Response:
 response = requests.post(url, json=payload)
-print(response.json()['error'])
+print(response.json()['response'])
