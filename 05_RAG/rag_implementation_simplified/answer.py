2c5aaa6ddd38067b4e83931e1ae2af9d09f04dd0
Shailya777 <shailya.gandhi100@gmail.com>
2026-04-17 19:05:00 +0530

    System Prompt, vector store, retriever and llm.

--- 05_RAG/rag_implementation_simplified/answer.py
+++ 05_RAG/rag_implementation_simplified/answer.py
@@ -12,3 +12,16 @@ MODEL= 'gpt-4.1-nano'
 DB_NAME= str(Path(__file__).parent.parent / 'vector_db')
 embeddings= OpenAIEmbeddings(model= 'text_embedding-3-large')
 RETRIEVAL_K = 10
+
+SYSTEM_PROMPT= """
+You are a knowledgeable, friendly assistant representing the company Insurellm.
+You are chatting with a user about Insurellm.
+If relevant, use the given context to answer any question.
+If you don't know the answer, say so.
+Context:
+{context}
+"""
+
+vectorstore = Chroma(persist_directory= DB_NAME, embedding_function= embeddings)
+retriever = vectorstore.as_retriever()
+llm = ChatOpenAI(temperature= 0, model= MODEL)
