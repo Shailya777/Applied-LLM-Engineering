7f9b9b0327aaab38d1c9ea35528b8688cf1d6c4b
Shailya777 <shailya.gandhi100@gmail.com>
2026-04-17 19:21:39 +0530

    Formatting and displaying context used by LLM to generate the response.

---
+++ 05_RAG/app.py
@@ -0,0 +1,22 @@
+import gradio as gr
+from dotenv import load_dotenv
+from rag_implementation_simplified.answer import answer_question
+load_dotenv()
+
+def format_context(context):
+    """
+    Converts a list of retrieved documents into a formatted HTML string for display, highlighting the source metadata and page content.
+
+    Args:
+    context (list): A list of Document objects containing metadata and text content.
+
+    Returns:
+    str: An HTML-formatted string featuring styled headers and source labels for each context fragment.
+    """
+
+    result = "<h2 style='color: #ff7800;'>Relevant Context</h2>\n\n"
+
+    for doc in context:
+        result += f"<span style='color: #ff7800;'>Source: {doc.metadata['source']}</span>\n\n"
+        result += doc.page_content + "\n\n"
+    return result
